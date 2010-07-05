f187423b4bdbfb100e98d926cad0cd23a1edecf0
7rans <transfire@gmail.com>
2010-07-05 09:17:00 -0400

    add syckle plugin

---
+++ lib/plugins/syckle/till.rb
@@ -0,0 +1,45 @@
+module Syckle::Plugins
+
+  # = Till Generation Service
+  #
+  class Till < Service
+
+    cycle :main, :generate
+    cycle :site, :generate
+
+    # Make automatic?
+    #autorun do
+    #  ...
+    #end
+
+    available do |project|
+      begin
+        require 'till'
+        true
+      rescue LoadError
+        false
+      end
+    end
+
+    #
+    #def safe?; @safe; end
+
+    #
+    def generate(options={})
+      options ||= {}
+
+      dir = nil # defaults to curent directory
+
+      options[:trial]  = trial?
+      options[:debug] = debug?
+      options[:quiet] = quiet?
+      options[:force] = force?
+
+      tiller = Till::Tiller.new(dir, options)
+      tiller.till
+    end
+
+  end
+
+end
+
